07bbc03b2a9db06bf04a16ee500e022fd08976df
Adrián Cantón Fernández <adriancanton@gmail.om>
2019-02-04 19:01:41 +0100

    Added WidgetsApp
    App to test radio buttons and checkboxes

---
+++ WidgetsApp/app/src/main/java/esadrcanfer/us/alumno/MainActivity.java
@@ -0,0 +1,31 @@
+package esadrcanfer.us.alumno;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void send(View view) {
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        TextView textView = findViewById(R.id.text2);
+
+        if(checkBox.isChecked() && radioGroup.getCheckedRadioButtonId() != -1){
+            throw new IllegalArgumentException();
+        } else if(checkBox.isChecked()){
+            textView.setText("Ha marcado el checkbox y pulsado el botón");
+        } else if(radioGroup.getCheckedRadioButtonId() != -1){
+            textView.setText("Ha marcado un radio button y pulsado el botón");
+        }
+    }
+}
